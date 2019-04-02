850f7287fd41b674bf5526e7003162fa5c3e0d30
Jdonovan87 <42076130+Jdonovan87@users.noreply.github.com>
2019-04-02 14:20:55 -0500

    updated some stuff

--- ACM.BL/AddressRepository.cs
+++ ACM.BL/AddressRepository.cs
@@ -32,6 +32,37 @@ namespace ACM.BL
             return address;
         }
 
+        public IEnumerable<Address> RetrieveByCustomerId(int customerId)
+        {
+            // Code that retrieves the defined addresses for the customer.
+
+            // Temporary hard-coded values to return a set of addresses for a customer
+            var addressList = new List<Address>();
+            Address address = new Address(1)
+            {
+                AddressType = 1,
+                StreetLine1 = "Bag End",
+                StreetLine2 = "Bagshot row",
+                City = "Hobbiton",
+                State = "Shire",
+                Country = "Middle Earth",
+                PostalCode = "144"
+            };
+            addressList.Add(address);
+
+            address = new Address(2)
+            {
+                AddressType = 2,
+                StreetLine1 = "Greeen Dragon",
+                City = "Bywater",
+                State = "Shire",
+                Country = "Middle Earth",
+                PostalCode = "146"
+            };
+
+            return addressList;
+        }
+
         public bool Save(Address address)
         {
             // Code that saves the passes in address
